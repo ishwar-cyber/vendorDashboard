6ca87bd8f484dc8cee69f63e052e92a5ce2c5be5
laveenchandwani <laveenchandwani25@gmail.com>
2024-10-05 23:59:10 +0530

    bug fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,3 @@
-import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
