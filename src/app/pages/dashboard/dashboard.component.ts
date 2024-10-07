231742950c738c7f5e2d76c2d18c3e52aa954a57
Nikhil More <nikhil.more1@bajajfinserv.in>
2024-10-07 23:35:03 +0530

    added booking tables for vendor and customer
    # Conflicts:
    #	angular.json
    #	package-lock.json
    #	src/app/app.config.ts
    #	src/app/component/sidebar/sidebar.component.ts
    #	src/app/pages/dashboard/dashboard.component.html
    #	src/app/pages/dashboard/dashboard.component.scss
    #	src/styles.scss

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../component/sidebar/sidebar.component';
 import { VendorHeaderComponent } from '../../component/vendor-header/vendor-header.component';
+import { VendorBookingResponse } from '../../interface/vendorBookingResponse';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet,VendorHeaderComponent,SidebarComponent],
+  imports: [RouterOutlet, VendorHeaderComponent, SidebarComponent,],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  vendorBookingResponse: VendorBookingResponse | undefined
+
+  constructor(private dashboardService: DashboardService) { }
+
+  currentDate: string | undefined;
+
+
+  ngOnInit(): void {
+    const date = new Date();
+    this.currentDate = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+    console.log(this.currentDate);
+    localStorage.setItem("vendorId", "VED6F946C980D45014");
+    this.dashboardService.getVendorBookings(localStorage.getItem("vendorId"), this.currentDate).subscribe((response) => {
+      this.vendorBookingResponse = response;
+      console.log(this.vendorBookingResponse);
+    })
+
+  }
+
+  onDateChange(event: any) {
+    console.log("event")
+    console.log(event);
+  }
+
+
 
 }
+
+
--- src/app/website/my-booking/my-booking.component.ts
+++ src/app/website/my-booking/my-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CustomerBookingResponse } from '../../interface/bookingResponse';
+import { MyBookingService } from './my-booking.service';
 
 @Component({
   selector: 'app-my-booking',
@@ -8,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrl: './my-booking.component.scss'
 })
 export class MyBookingComponent {
+
+  srNo = 0;
+
+  customerId: string = "abc";
+
+  constructor(private myBookingservice: MyBookingService) { };
+
+  customerBookingResponse: CustomerBookingResponse | undefined
+
+  ngOnInit(): void {
+
+    localStorage.setItem("customerId", "CU2E200075A50B618E");
+    localStorage.setItem("token","pqr")
+    console.log(localStorage.getItem("token"))
+    // if (localStorage.getItem("customerId")!=undefined ||null ) {
+      // this.customerId = localStorage.getItem("customerId");
+    // }
+    this.myBookingservice.getCustomerBookings(localStorage.getItem("customerId")).subscribe((response) => {
+      this.customerBookingResponse = response
+      console.log(this.customerBookingResponse);
+    })
+  }
+
+  deleteBooking(customerid: any, index:any) {
+    this.myBookingservice.deleteBooking(customerid).subscribe((response) => {
+      console.log(customerid)
+      console.log(response);
+    })
+    // throw new Error('Method not implemented.');
+  }
+
+  srNoUpdate(){
+    this.srNo=this.srNo+1
+  }
 
 }
